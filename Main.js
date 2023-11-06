92870c19fce8ed6339d13b67d36a14ddb11571f2
ayadfadhil <ayad.jumaah@gmail.com>
2023-11-06 11:32:04 +0100

    added a logo and a css file

--- Main.js
+++ Main.js
@@ -4,13 +4,6 @@ const Main = () => {
   return (
     <div>
       <h1>React App</h1>
-      <p>React App</p>
-      {console.log(object)}
-      <h2>React App</h2>
-      <h3>React App</h3>
-      <p>
-        <a href="sdfsdfsdf">click here</a>
-      </p>
     </div>
   );
 };
